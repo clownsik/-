f6f1d8267612bb64b65d7ad746fa378c86815463
ivan <ivanov@mpt.ru>
2022-05-21 18:13:41 +0300

    aye

---
+++ UserRegistration/PasswordChecker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public static class PasswordChecker
+    {
+        public static int GetPasswordStrenght(string passsword)
+        {
+            int result = 0;
+
+            if (Math.Max(passsword.Length, 7)>7)
+            {
+                result++;
+            }
+
+            
+            return result;
+        }
+        public static int GetPasswordLower(string passsword)
+        {
+            int result = 0;
+            foreach (char c in passsword)
+            {
+                if ("qwertyuiopasdfghjklzxcvbnm".Contains(c))
+                {
+                    result++;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public static int GetPasswordUpper(string passsword)
+        {
+            int result = 0;
+            foreach (char c in passsword)
+            {
+                if ("QWERTYUIOPASDFGHJKLZXCVBNM".Contains(c))
+                {
+                    result++;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public static int GetPasswordNumber(string passsword)
+        {
+            int result = 0;
+            foreach (char c in passsword)
+            {
+                if (Char.IsDigit(c))
+                {
+                    result++;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public static int GetPasswordSpecial(string passsword)
+        {
+            int result = 0;
+            foreach (char c in passsword)
+            {
+                if ("!@#%&*$?/~".Contains(c))
+                {
+                    result++;
+                    break;
+                }
+            }
+            return result;
+        }      
+    }
+}
---
+++ UserRegistrationTest/PasswordStrenghtCheckerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UserRegistration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class PasswordStrenghtCheckerTests
+    {
+        [TestMethod]
+        public void GetPassword_7_1point()
+        {
+            //arrange
+            string password = "Ivan00649393!";
+
+            int excpected = 1;
+            //act
+            int actual = PasswordChecker.GetPasswordStrenght(password);
+            //assert
+            Assert.AreEqual(excpected, actual);
+        }
+
+        [TestMethod]
+        public void GetPassword_7_2point()
+        {
+            //arrange
+            string password = "Ivan00649393!";
+            int excpected = 1;
+            //act
+            int actual = PasswordChecker.GetPasswordLower(password);
+            //assert
+            Assert.AreEqual(excpected, actual);
+        }
+
+        [TestMethod]
+        public void GetPassword_7_3point()
+        {
+            //arrange
+            string password = "Ivan00649393!";
+            int excpected = 1;
+            //act
+            int actual = PasswordChecker.GetPasswordUpper(password);
+            //assert
+            Assert.AreEqual(excpected, actual);
+        }
+
+        [TestMethod]
+        public void GetPassword_7_4point()
+        {
+            //arrange
+            string password = "Ivan00649393!";
+            int excpected = 1;
+            //act
+            int actual = PasswordChecker.GetPasswordNumber(password);
+            //assert
+            Assert.AreEqual(excpected, actual);
+        }
+
+        [TestMethod]
+        public void GetPassword_7_5point()
+        {
+            //arrange
+            string password = "Ivan00649393!";
+            int excpected = 1;
+            //act
+            int actual = PasswordChecker.GetPasswordSpecial(password);
+            //assert
+            Assert.AreEqual(excpected, actual);
+        }
+    }
+}
